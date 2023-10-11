416ceea573630ede36bbbcecebafda24aa3e1945
Ameen_Works <ameen.desk@gmail.com>
2023-10-11 20:15:56 +0530

    Updated wordings

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,61 +1,103 @@
-import React from 'react'
-import "./Contact.css"
-import { MdOutlineMail } from "react-icons/md"
-import { RiMessengerLine } from "react-icons/ri"
-import { FaWhatsapp } from "react-icons/fa"
-import { useRef } from 'react';
-import emailjs from "emailjs-com"
-
+import React from "react";
+import "./Contact.css";
+import { MdOutlineMail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm('service_tlzfp45', 'template_w0lg9nb', form.current, 'DoAGMwYqrBIOv574u')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            })
-        e.target.reset();
-    };
-    return (
-        <section id='contact'>
-            <h5>Get in Touch</h5>
-            <h2>Contat Me</h2>
+    emailjs
+      .sendForm(
+        "service_tlzfp45",
+        "template_w0lg9nb",
+        form.current,
+        "DoAGMwYqrBIOv574u"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
 
-            <div className="container contact__container">
-                <div className="contact__options">
-                    <article className="contact__option">
-                        <MdOutlineMail className='contact__option-icon' />
-                        <h4>Email</h4>
-                        <h5>ameen.desk@gmail.com</h5>
-                        <a href='mailt:ameen.desk@gmail.com' target='_blank' rel='noreferrer'> Send a message</a>
-                    </article>
-                    <article className="contact__option">
-                        <RiMessengerLine className='contact__option-icon' />
-                        <h4>Messenger</h4>
-                        <h5>Ameen_Works</h5>
-                        <a href='https://m.me/arshath.ameen/' target='_blank' rel='noreferrer'> Send a message</a>
-                    </article>
-                    <article className="contact__option">
-                        <FaWhatsapp className='contact__option-icon' />
-                        <h4>WhatsApp</h4>
-                        <h5>Ameen_Works</h5>
-                        <a href='https://wa.me/+918124934431' target='_blank' rel='noreferrer'> Send a message</a>
-                    </article>
-                </div>
-                <form ref={form} onSubmit={sendEmail}>
-                    <input type="text" name='name' placeholder='Your Full Name' required />
-                    <input type="text" name='email' placeholder='Your Email' required />
-                    <textarea name='message' rows="7" placeholder='Your Message' required />
-                    <button type="submit" className='btn btn-primary'> Send Message</button>
-                </form>
-            </div>
-        </section>
-    )
-}
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>ameen.desk@gmail.com</h5>
+            <a
+              href="mailto:ameen.desk@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Ameen_Works</h5>
+            <a
+              href="https://m.me/arshath.ameen/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>Ameen_Works</h5>
+            <a
+              href="https://wa.me/+918124934431"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            {" "}
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
